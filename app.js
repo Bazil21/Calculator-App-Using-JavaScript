e4ab0068e7de5539c1fc95d7aa5fdf05441201f9
Bazil21 <59576110+Bazil21@users.noreply.github.com>
2020-10-03 06:49:58 -0700

    Updated

---
+++ app.js
@@ -0,0 +1,21 @@
+
+  AOS.init();
+
+
+var result = document.getElementById("result");
+
+function getnum(num){
+    result.value += num;
+}
+
+function clearResult(){
+    result.value = "";
+}
+
+function getResult()
+{
+    result.value = eval(result.value);
+}
+function getpow(){
+    result.value = Math.pow(num)
+}
